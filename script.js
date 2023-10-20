b5ab710e54cf3508c86df10474730c8baefc31a8
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2023-10-20 13:21:35 -0400

    added all possible characters

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+function generatePassword() {
+  const numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+  const uppercaseCharacters =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const lowercaseCharacters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const specialCharacters =['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  const possibleCHaracters=[];
+
+
+ 
+const generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
